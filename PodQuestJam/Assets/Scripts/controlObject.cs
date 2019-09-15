4c5085e09868bdaf47ffa3238469e3d941969e07
Mauridio <mauricio.s.marra@gmail.com>
2019-09-15 19:06:35 -0300

    Commit inicial com o projeto.
    Projeto com inicial com algumas ações básicas (mover, pegar, depositar em baú).

---
+++ PodQuestJam/Assets/Scripts/controlObject.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controlObject : MonoBehaviour
+{
+    private bool isHolding = false;
+    private GameObject objectToHoldDrop;
+    private GameObject chestToDrop;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (isHolding)
+            {
+                Drop();
+            }
+            else
+            {
+                PickUp();
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (!objectToHoldDrop)
+                objectToHoldDrop = other.gameObject;
+        }
+
+        if (other.tag == "Chest")
+            chestToDrop = other.gameObject;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (!isHolding)
+            {
+                objectToHoldDrop = null;
+            }
+        }
+    }
+
+    private void PickUp()
+    {
+        if (!objectToHoldDrop)
+            return;
+
+        this.transform.SetParent(objectToHoldDrop.transform);
+        this.transform.position = objectToHoldDrop.transform.GetChild(0).position;
+        isHolding = true;
+    }
+
+    private void Drop()
+    {
+        if (chestToDrop)
+            Destroy(this.gameObject);
+
+        this.transform.SetParent(null);
+        isHolding = false;
+    }
+}
---
+++ PodQuestJam/Assets/Scripts/controlPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controlPlayer : MonoBehaviour
+{
+    private Rigidbody myRigidBody;
+    private float positionX;
+    private float positionY;
+    private Vector3 position;
+    public float mySpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        positionX = Input.GetAxis("Horizontal");
+        positionY = Input.GetAxis("Vertical");
+        position = new Vector3(positionX, 0, positionY);
+        myRigidBody.MovePosition(myRigidBody.position + position * Time.deltaTime * mySpeed);
+
+        myRigidBody.rotation = Quaternion.LookRotation(position, Vector3.zero);
+    }
+
+}
